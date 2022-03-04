52e822b8a162c55ddd82a78807d27653f88fa821
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-03-04 11:42:54 -0500

    added styling to account for Search component

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,11 @@
-
+import Search from "./Search"
 
 const Body = () => {
   return (
-    <div>Body</div>
+      // flex-grow allows the Body - middle section to take up as much space as possible
+    <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5"> 
+        <Search />
+        </section>
   )
 }
 
