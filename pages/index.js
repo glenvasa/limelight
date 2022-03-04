c45fefad4015ebe9efe735facc1cb4f6bc3edecc
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-03-04 09:40:43 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>LimeLight</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
